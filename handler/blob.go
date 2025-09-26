68f1b8ea448e89e8b75c9221299686313d1a63c8
zhuyanhuazhuyanhua <zhuyanhuazhuyanhua@qq.com>
2025-09-26 14:07:45 +0800

    feat:add internal

--- handler/blob.go
+++ handler/blob.go
@@ -4,10 +4,10 @@ import (
 	"net/http"
 
 	"github.com/distribution/distribution/v3"
-	"github.com/distribution/distribution/v3/internal/dcontext"
 	"github.com/distribution/distribution/v3/registry/api/errcode"
 	"github.com/gorilla/handlers"
 	"github.com/opencontainers/go-digest"
+	"github.com/zhuyanhuazhuyanhua/docker-registry/internal/dcontext"
 )
 
 // blobDispatcher uses the request context to build a blobHandler.
