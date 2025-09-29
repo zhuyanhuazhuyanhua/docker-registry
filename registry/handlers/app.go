d3f69376bcad780a7b10586854417012fec31a04
zhuyanhuazhuyanhua <zhuyanhuazhuyanhua@qq.com>
2025-09-29 11:16:33 +0800

    feat:fix bugs

---
+++ registry/handlers/app.go
@@ -0,0 +1,48 @@
+package handlers
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/distribution/distribution/v3"
+	"github.com/distribution/distribution/v3/configuration"
+	"github.com/distribution/distribution/v3/notifications"
+	"github.com/distribution/distribution/v3/registry/auth"
+	storagedriver "github.com/distribution/distribution/v3/registry/storage/driver"
+	"github.com/docker/go-events"
+	"github.com/gorilla/mux"
+	"github.com/redis/go-redis/v9"
+)
+
+// App is a global registry application object. Shared resources can be placed
+// on this object that will be accessible from all requests. Any writable
+// fields should be protected.
+type App struct {
+	context.Context
+
+	Config *configuration.Configuration
+
+	router           *mux.Router                    // main application router, configured with dispatchers
+	driver           storagedriver.StorageDriver    // driver maintains the app global storage driver instance.
+	registry         distribution.Namespace         // registry is the primary registry backend for the app instance.
+	repoRemover      distribution.RepositoryRemover // repoRemover provides ability to delete repos
+	accessController auth.AccessController          // main access controller for application
+
+	// httpHost is a parsed representation of the http.host parameter from
+	// the configuration. Only the Scheme and Host fields are used.
+	httpHost url.URL
+
+	// events contains notification related configuration.
+	events struct {
+		sink   events.Sink
+		source notifications.SourceRecord
+	}
+
+	redis redis.UniversalClient
+
+	// isCache is true if this registry is configured as a pull through cache
+	isCache bool
+
+	// readOnly is true if the registry is in a read-only maintenance mode
+	readOnly bool
+}
---
+++ registry/handlers/context.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/distribution/distribution/v3"
+	"github.com/opencontainers/go-digest"
+	"github.com/zhuyanhuazhuyanhua/docker-registry/internal/dcontext"
+	"github.com/zhuyanhuazhuyanhua/docker-registry/registry/api/errcode"
+	v2 "github.com/zhuyanhuazhuyanhua/docker-registry/registry/api/v2"
+)
+
+// Context should contain the request specific context for use in across
+// handlers. Resources that don't need to be shared across handlers should not
+// be on this object.
+type Context struct {
+	// App points to the application structure that created this context.
+	*App
+	context.Context
+
+	// Repository is the repository for the current request. All requests
+	// should be scoped to a single repository. This field may be nil.
+	Repository distribution.Repository
+
+	// RepositoryRemover provides method to delete a repository
+	RepositoryRemover distribution.RepositoryRemover
+
+	// Errors is a collection of errors encountered during the request to be
+	// returned to the client API. If errors are added to the collection, the
+	// handler *must not* start the response via http.ResponseWriter.
+	Errors errcode.Errors
+
+	urlBuilder *v2.URLBuilder
+
+	// TODO(stevvooe): The goal is too completely factor this context and
+	// dispatching out of the web application. Ideally, we should lean on
+	// context.Context for injection of these resources.
+}
+
+var errDigestNotAvailable = fmt.Errorf("digest not available in context")
+
+func getDigest(ctx context.Context) (dgst digest.Digest, err error) {
+	dgstStr := dcontext.GetStringValue(ctx, "vars.digest")
+
+	if dgstStr == "" {
+		dcontext.GetLogger(ctx).Errorf("digest not available")
+		return "", errDigestNotAvailable
+	}
+
+	d, err := digest.Parse(dgstStr)
+	if err != nil {
+		dcontext.GetLogger(ctx).Errorf("error parsing digest=%q: %v", dgstStr, err)
+		return "", err
+	}
+
+	return d, nil
+}
